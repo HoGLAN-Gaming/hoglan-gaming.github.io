64853c03ef89124df069fcf3c6a5bf42f184c99c
Chris Hunt <chris.hunt@epigenesys.co.uk>
2015-08-05 09:56:55 +0100

    add extra strap

--- app/assets/javascript/hoglan.js
+++ app/assets/javascript/hoglan.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('.randomise p').addClass('hide');
-  random_num = Math.floor((Math.random() * 7));
+  random_num = Math.floor((Math.random() * 8));
   $('.randomise p').eq(random_num).removeClass('hide');
 });
